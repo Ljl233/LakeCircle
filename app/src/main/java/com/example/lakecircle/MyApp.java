17975fde74f7a362068761679f01fde07cfbef16
Ljl233 <2955441566@qq.com>
2020-02-08 17:41:41 +0800

    add SPUtils

---
+++ app/src/main/java/com/example/lakecircle/MyApp.java
@@ -0,0 +1,23 @@
+package com.example.lakecircle;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+public class MyApp extends Application {
+    private static Context applicationContext;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext=this.getApplicationContext();
+        Fresco.initialize(this);
+    }
+
+
+    //一般不要随便使用,只在某些需要用到的全局方法里使用
+    public static Context getAppContext(){
+        return applicationContext;
+
+    }
+}

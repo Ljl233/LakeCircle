076b2629d8ca71b3b487febba6666b0c3e43ace5
Ljl233 <2955441566@qq.com>
2020-02-16 18:07:32 +0800

    home ui

--- app/src/main/java/com/example/lakecircle/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lakecircle/ui/home/HomeFragment.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.example.lakecircle.R;
 
 public class HomeFragment extends Fragment {
+
+    private SearchView mSearchView;
+    private TextView mTvAddress, mTvTimeLake, mTvTravel, mTvLakeWalk, mTvLightLake;
+    private ImageView mIvProblem, mIvActivity, mIvMerchant, mIvNotice;
 
     @Nullable
     @Override
@@ -19,7 +26,54 @@ public class HomeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        mSearchView = view.findViewById(R.id.searchView);
+        mTvAddress = view.findViewById(R.id.home_address);
+        mTvLakeWalk = view.findViewById(R.id.home_tv_lakewalk);
+        mTvLightLake = view.findViewById(R.id.home_tv_lightlake);
+        mTvTimeLake = view.findViewById(R.id.home_tv_timelake);
+        mTvTravel = view.findViewById(R.id.home_tv_travel);
+        mIvActivity = view.findViewById(R.id.home_iv_detail);
+        mIvMerchant = view.findViewById(R.id.home_iv_merchant);
+        mIvProblem = view.findViewById(R.id.home_iv_problem);
+        mIvNotice = view.findViewById(R.id.home_iv_notice);
+
+        initView();
 
         return view;
+    }
+
+    private void initView() {
+
+        mTvAddress.setOnClickListener(v -> {
+
+        });
+
+        mTvLakeWalk.setOnClickListener(v -> {
+
+        });
+
+        mTvLightLake.setOnClickListener(v -> {
+
+        });
+
+        mTvTimeLake.setOnClickListener(v -> {
+
+        });
+
+        mTvTravel.setOnClickListener(v -> {
+
+        });
+
+        mIvActivity.setOnClickListener(v -> {
+
+        });
+
+        mIvMerchant.setOnClickListener(v -> {
+
+        });
+
+        mIvProblem.setOnClickListener(v -> {
+
+        });
     }
 }
